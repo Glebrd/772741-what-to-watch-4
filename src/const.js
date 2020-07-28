423bc79b5d00966cb27e00e74641befa4b16a448
Gleb Dolgushin <dolgushing@gmail.com>
2020-07-29 02:10:53 +0300

    31. TypeScript спешит на помощь правки 20

--- src/const.js
+++ src/const.js
@@ -32,16 +32,4 @@ export const AppRoute = {
   MY_LIST: `/mylist`,
 };
 
-export const MovieRank = {
-  MOVIE: `/films/:id`,
-  MOVIES: `/films/`,
-  PLAYER_PAGE: `/films/:id/player`,
-  PLAYER: `/player`,
-  REVIEW_PAGE: `/films/:id/review`,
-  REVIEW: `/review`,
-  LOGIN: `/login`,
-  ROOT: `/`,
-  MY_LIST: `/mylist`,
-};
-
 
