e851f6977da747c00a6d1365ecc5ec59af5ffabe
Gleb Dolgushin <dolgushing@gmail.com>
2020-07-29 01:04:39 +0300

    31. TypeScript спешит на помощь правки 18

--- src/api.js
+++ src/api.js
@@ -2,13 +2,13 @@ import axios from "axios";
 import history from "./history";
 import {AppRoute} from "./const";
 
-const Error = {
-  UNAUTHORIZED: 401
-};
-
 const BASE_URL = `https://4.react.pages.academy/wtw`;
 const TIMEOUT = 5000;
 const ADD_TO_FAVORITE_REQUEST_PATH = `favorite`;
+
+const Error = {
+  UNAUTHORIZED: 401
+};
 
 export const createAPI = () => {
   const api = axios.create({
